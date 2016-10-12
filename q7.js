f86e56a6f2685995f1e46a1670bf8c6aa5b5ff3d
Daniel Sahu <danielmohansahu@gmail.com>
2016-10-12 13:16:27 -0400

    added radio button filters

--- q7.js
+++ q7.js
@@ -7,7 +7,8 @@ var margin = {"top":50, "bottom":10, "left":75, "right":75},
 	menu_width = width*2/3;
 	menu_padding_x = 25,
 	menu_padding_y = 15,
-	menu_height = menu_padding_y*10;
+	menu_height = menu_padding_y*10,
+	menu_width = menu_padding_x*15;
 
 // D3 Transition
 var t = d3.transition()
@@ -17,6 +18,7 @@ var t = d3.transition()
 var SELECTED_STATES = ['GA'],
 	SELECTED_X = 'SAT Score',
 	SELECTED_Y = 'Student Debt ($)';
+	SELECTED_CHECKBOX = [];
 
 var data_dict = {
 	'ACT Score (25th %)':'ACTCM25',
@@ -31,6 +33,20 @@ var data_dict = {
 	'Undergraduate Enrollment':'UGDS',
 	'10 Year Median Salary ($)':'md_earn_wne_p10',
 }
+
+var checkbox_dict = {
+	'Women Only':'WOMENONLY',
+	'Men Only':'MENONLY',
+	'Public Only':'PUBLIC',
+	'Private ONly':'PRIVATE',
+	'Historically Black':'HBCU',
+	'Primarily Black':'PBI',
+	'Tribal':'TRIBAL',
+	'Native American (non Tribal)':'NANTI',
+	'Alaska Native/Native Hawaiian':'ANNHI',
+	'Asian American/Pacific Islander':'AANAPII'
+}
+
 var state_dict = {
 	'AK':'Alaska','AL':'Alabama','AR':'Arkansas','AS':'American Samoa',
 	'AZ':'Arizona','CA':'California','CO':'Colorado','CT':'Connecticut','DC':'District of Columbia',
@@ -60,6 +76,7 @@ var svg = d3.select("body")
 	.attr("height",height + menu_height + margin.top + margin.bottom)
 	.attr("transform","translate(" + margin.left + "," + (menu_height + margin.top) + ")")
 
+// Selects:
 var dropDownSTATE = menu.append("div")
 	.style("position","relative")
 	.style("top","" + menu_padding_y + "px")
@@ -106,13 +123,37 @@ menu.append("div")
 	.append("text")
 	.text("Y Axis:")
 
+// Checkbox Buttons:
+var i = 0;
+var checkbox_buttons = [];
+for (key in checkbox_dict) {
+	checkbox_buttons.push(
+		menu.append("input")
+		.attr("type","checkbox")
+		.on("click",checkbox_click)
+		.attr("name",key)
+		.style("position","absolute")
+		.style("top","" + menu_padding_y*(1+i) + "px")
+		.style("left","" + menu_width + "px")
+	)
+	menu.append("div")
+		.style("position","absolute")
+		.style("top","" + menu_padding_y*(1+i) + "px")
+		.style("left","" + (menu_width + menu_padding_x) + "px")
+		.append("text")
+		.style("font-size","14px")
+		.text(key)
+	i++;
+}
+
 /////////////// INITIALIZE ///////////////
+start_inputs();
 data_loop('initialize');
 
 /////////////// CRITICAL FUNCTIONS ///////////////
 
-// Data Loop:
-function data_loop(input,selectValue,initialize) {
+// Initialize Buttons/Drop Downs
+function start_inputs() {
 	d3.csv("Most+Recent+Cohorts+(Scorecard+Elements).csv",function(error, data) { 
 		if (error) throw error;
 
@@ -148,9 +189,17 @@ function data_loop(input,selectValue,initialize) {
 			.attr("class","form-control")
 			.style("width", "" + select_width + "px")
 			.style("height", "25px")
- 
+	} );
+};
+
+// Data Loop:
+function data_loop(input,selectValue,initialize) {
+	d3.csv("Most+Recent+Cohorts+(Scorecard+Elements).csv",function(error, data) { 
+		if (error) throw error;
 
 		if (input === 'initialize') {
+			make_scatter(data,SELECTED_X,SELECTED_Y,SELECTED_STATES);}
+		else if (input === 'checkbox') {
 			make_scatter(data,SELECTED_X,SELECTED_Y,SELECTED_STATES);}
 		else if (input === 'STATE') {
 			make_scatter(data,SELECTED_X,SELECTED_Y,selectValue);}
@@ -171,15 +220,7 @@ function make_scatter(data,x_param,y_param,states) {
 		plot.selectAll("g").remove(); }
 
 	// Filter by state and filter our NULL data
-	data = data.filter(function(d) {
-		var conditional = false;
-		for (state in states) {
-			conditional = conditional || (d['STABBR']===states[state]); }
-		return conditional; })
-	data = data.filter(function(d) {return d[data_dict[x_param]] !== 'NULL';})
-	data = data.filter(function(d) {return d[data_dict[x_param]] !== 'PrivacySuppressed';})
-	data = data.filter(function(d) {return d[data_dict[y_param]] !== 'NULL';})
-	data = data.filter(function(d) {return d[data_dict[y_param]] !== 'PrivacySuppressed';})
+	data = filter_data(data,x_param,y_param,states);
 
 	// Scales:
 	var x = d3.scale.linear().domain(get_domain(data,data_dict[x_param],0.1,0.1)).rangeRound([padding,width-padding]),
@@ -279,6 +320,41 @@ function make_scatter(data,x_param,y_param,states) {
 
 /////////////// HELPER FUNCTIONS ///////////////
 
+function filter_data (data,x_param,y_param,states) {
+	SELECTED_CHECKBOX = [];
+
+	get_checked(checkbox_buttons).forEach(function(d) {
+		SELECTED_CHECKBOX.push(checkbox_dict[checkbox_buttons[d].property('name')]);
+	});
+
+	data = data.filter (function(d) {
+		var conditional = true;
+		for (var i = 0; i<SELECTED_CHECKBOX.length;i++) {
+			if (SELECTED_CHECKBOX[i] === 'PUBLIC') {
+				conditional = conditional && (d['NPT4_PUB'] !== 'NULL'); }
+			else if (SELECTED_CHECKBOX[i] === 'PRIVATE') {
+				conditional = conditional && (d['NPT4_PRIV'] !== 'NULL'); }
+			else {
+				conditional = conditional && (d[SELECTED_CHECKBOX[i]]==1); }
+		}
+		return conditional;	});
+	data = data.filter(function(d) {
+		var conditional = false;
+		for (state in states) {
+			conditional = conditional || (d['STABBR']===states[state]); }
+		return conditional; })
+	data = data.filter(function(d) {return d[data_dict[x_param]] !== 'NULL';})
+	data = data.filter(function(d) {return d[data_dict[x_param]] !== 'PrivacySuppressed';})
+	data = data.filter(function(d) {return d[data_dict[y_param]] !== 'NULL';})
+	data = data.filter(function(d) {return d[data_dict[y_param]] !== 'PrivacySuppressed';})
+
+	return data;
+}
+
+function checkbox_click() {
+	data_loop('checkbox');
+}
+
 function onchangeSTATE() {
 	SELECTED_STATES = []
 	getSelected(dropDownSTATE).forEach(function(d) {
@@ -300,6 +376,16 @@ function getSelected(select) {
 		if (options[opt].selected) {
 			result.push(options[opt].value); } }
 	return result; }
+
+function get_checked(checkbox) {
+	index = [];
+	checkbox.forEach(function(d,i) {
+		if (d.property('checked')) {
+			index.push(i);
+		}
+	})
+	return index;
+}
 
 function gen_title(x_param,y_param,states) {
 	var title = y_param + " vs. " + x_param + ' for ';
